4e462ae235a0b400b46adbeac2d98cbdd5f44667
Johannes Herrmann <johannes@jhlabs.io>
2021-01-31 14:52:20 +0100

    updated up to caching lecture

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,44 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
+import * as esbuild from "esbuild-wasm";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define: {
+        "process.env.NODE_ENV": '"production"',
+        global: "window",
+      },
+    });
+
+    // console.log(result);
+
+    setCode(result.outputFiles[0].text);
+  };
 
   return (
     <div>
@@ -16,7 +51,7 @@ const App = () => {
         {input}
       </textarea>
       <div>
-        <button type="submit">Submit</button>
+        <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
     </div>
